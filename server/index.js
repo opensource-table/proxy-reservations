92ebc4e5afe7aa98339b30eb4ff9e754223be2b4
Erik Soderholm <esodey@gmail.com>
2019-06-17 11:16:59 -0700

    Use k6 and newrelic to stress test the proxy server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
@@ -12,6 +14,20 @@ app.get('/:id', (req, res) => {
     res.sendFile('index.html', { root: path.resolve(__dirname, '../public') });
   }
 });
+
+app.get('/:id/reservations', (req, res) => {
+  // console.log("i am inside of get")
+  const resID = Number(req.params.id);
+  axios.get(`http://localhost:3020/${resID}/reservations`)
+  .then(response => {
+    res.status(200).send(response.data);
+  })
+  .catch(error => {
+    res.status(500).send();
+  })
+});
+
+
 
 app.listen(3000, () => {
   console.log('Open Table proxy server listening on port 3000!');
---
+++ server/k6Script.js
@@ -0,0 +1,35 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Model } from "mongoose";
+
+export let options = {
+  vus: 200,
+  rps: 200,
+  duration: "3m",
+};
+
+let min = 8000000;
+let max = 10000000;
+
+
+export default function() {
+    let iterationStart = new Date().getTime(); // timestamp in ms
+  
+    // let randomID = Math.floor(Math.random() * 10000000);
+      let res = http.get(`http://localhost:3000/${Math.floor(Math.random()*((max-min) + min))}/reservations`);
+  
+    let iterationDuration = (new Date().getTime() - iterationStart) / 1000;
+    let sleepTime = 1 - iterationDuration;  // 1 second minus time spent on request execution
+    
+    check(res, {
+        "status was 200": (r) => r.status == 200,
+        "transaction time OK": (r) => r.timings.duration < 200
+      });
+
+    if(sleepTime > 0){
+      sleep(sleepTime);
+    }
+  };
+
+  // k6 run server/k6Script.js
+
